d7dbceb960f8f97183d898f0661f4b70c062adfc
Hassan Ali <hassan.ali@empired.com>
2020-06-30 14:13:08 +1000

    add some simple styles and colours

--- App.js
+++ App.js
@@ -1,22 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-export default function App() {
+const colours = [
+  'Cyan: #2aa198',
+  'Blue: #268bd2',
+  'Magenta: #d33682',
+  'Orange: #cb4b16',
+];
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! :)</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safe}>
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Here are some boxes of different colours
+        </Text>
+
+        {colours.map((color) => {
+          const [name, hex] = color.split(':');
+          return (
+            <View style={[{ backgroundColor: hex.trim() }, styles.colours]}>
+              <Text style={{ color: '#fff' }}>{name}</Text>
+            </View>
+          );
+        })}
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+  },
+  text: {
+    fontWeight: '700',
+  },
+  colours: {
+    marginVertical: 3,
+    padding: 5,
+    borderRadius: 2,
+    color: '#ffffff',
+  },
   container: {
     flex: 1,
-    color: '#fff',
-    backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    paddingHorizontal: 20,
   },
 });
+
+/*{ <View style={styles.container}>
+
+        <Text>Open up App.js to start working on your app! :)</Text>
+      </View> }*/
+
+export default App;
+
+/*
+
+ flex: 1,
+    backgroundColor: '#f00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  */
