7cd27475cc0c5aa6d4c41dce7279bf63787e4770
Hassan Ali <hassan.ali@empired.com>
2020-07-01 14:43:05 +1000

    add basic container

--- patterns/screens/colorPalette.js
+++ patterns/screens/colorPalette.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
+// Local
 import { Palette } from '@molecules/palette';
 import { COLORS } from '@root/constants';
 
 export const ColorPalette = (props) => (
-  <Palette title="Solarised" colors={COLORS} />
+  <View style={styles.container}>
+    <Palette title="Solarised" colors={COLORS} />
+  </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#FFF',
+  },
+});
 
 export default ColorPalette;
--- patterns/screens/home.js
+++ patterns/screens/home.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, StyleSheet } from 'react-native';
 
 export const Home = ({ navigation }) => (
-  <View>
+  <View style={styles.container}>
     <TouchableOpacity onPress={() => navigation.push('ColorPalette')}>
       <Text>Solarized</Text>
     </TouchableOpacity>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#FFF',
+  },
+});
 
 export default Home;
