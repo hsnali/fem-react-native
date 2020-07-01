2c304d3d25bfee603476cc139af74dd3232f5aec
Hassan Ali <hassan.ali@empired.com>
2020-07-01 14:37:28 +1000

    start adding stack navigation

--- App.js
+++ App.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { Palette } from '@molecules/palette';
-import { COLORS } from './constants';
+// Local
+import { Home } from '@screens/home';
+import { ColorPalette } from '@screens/colorPalette';
+
+// Constants
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.safe}>
-      <View style={styles.container}>
-        <Text style={styles.bold}>
-          Here are some boxes of different colours
-        </Text>
-        <Palette title="Solarized" colors={COLORS} />
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ColorPalette" component={ColorPalette} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  safe: {
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-    paddingTop: 30,
-    paddingHorizontal: 20,
-  },
-  bold: {
-    fontWeight: '700',
-  },
-});
+// const styles = StyleSheet.create({
+//   safe: {
+//     flex: 1,
+//   },
+//   container: {
+//     flex: 1,
+//     paddingTop: 30,
+//     paddingHorizontal: 20,
+//   },
+//   bold: {
+//     fontWeight: '700',
+//   },
+// });
 
 export default App;
