1f3e172dd6c054e9a6d50f6e8462ecadc5d81547
Hassan Ali <hassan.ali@empired.com>
2020-07-03 17:04:59 +1000

    enable entire row click

--- patterns/03-molecules/colorToggle.js
+++ patterns/03-molecules/colorToggle.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import { View, Text, Switch } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Switch, TouchableOpacity } from 'react-native';
 
 // Local
 import { utilStyles } from '@base/styles/utils';
 import { Thumbnail } from '@atoms/thumbnail';
 
-export const ColorToggle = ({ colorName, hexCode }) => (
-  <View style={utilStyles.switchFields}>
-    <View style={utilStyles.rowFieldMinimal}>
-      <Thumbnail color={hexCode} style={utilStyles.thumbnailSmall} />
-      <Text>{colorName}</Text>
-    </View>
-    <Switch />
-  </View>
-);
+export const ColorToggle = ({ colorName, hexCode }) => {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  return (
+    <TouchableOpacity onPress={() => setIsEnabled(!isEnabled)}>
+      <View style={utilStyles.switchFields}>
+        <View style={utilStyles.rowFieldMinimal}>
+          <Thumbnail color={hexCode} style={utilStyles.thumbnailSmall} />
+          <Text>{colorName}</Text>
+        </View>
+        <Switch
+          value={isEnabled}
+          onValueChange={() => setIsEnabled(!isEnabled)}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 export default ColorToggle;
